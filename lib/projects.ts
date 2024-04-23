1a67cef90f37f2dc859bd613848170a110b5d506
Pedro Cruz <pedropcruz@outlook.pt>
2024-04-23 10:30:16 +0100

    fix(): update projects descriptions

--- lib/projects.ts
+++ lib/projects.ts
@@ -2,14 +2,14 @@ export const projects = [
 	{
 		title: 'Wedding MIP',
 		caption: 'Wedding Maria Inês e Pedro',
-		smallDescription: 'Wedding website for one of my biggest friend ever.',
+		smallDescription: 'Wedding website for one of my biggest friend ever',
 		link: 'https://wedding-mip.netlify.app/login',
 		src: '/preview-wedding-mip.jpg',
 	},
 	{
 		title: 'Oficina do comportamento',
-		caption: 'Wedding Maria Inês e Pedro',
-		smallDescription: `"Oficina do Comportamento" is a website featuring the services of three therapists specializing in children's behavioral therapy.`,
+		caption: `Child behavioral therapy experts' hub.`,
+		smallDescription: `"Oficina do Comportamento" is a website featuring the services of three therapists specializing in children's behavioral therapy`,
 		link: 'https://oficinadocomportamento.pt/',
 		src: '/preview-oficinadocomportamento.jpg',
 	},
@@ -17,15 +17,15 @@ export const projects = [
 		title: 'Devsplan',
 		caption: 'Portuguese programming podcast',
 		smallDescription:
-			'Devsplan, the podcast where three seasoned developers come together to explore the intricate world of programming.',
+			'Devsplan, the podcast where three seasoned developers come together to explore the intricate world of programming',
 		link: 'https://devsplan.pt/',
 		src: '/preview-devsplan.jpg',
 	},
 	{
 		title: 'Propecuária - myProvet',
-		caption: 'Take your simracing sport to the next level',
+		caption: 'Pharmacy platform focused on veterinarians',
 		smallDescription:
-			'Platform for pharmacies, where the product niche was largely dedicated to animals and veterinarians',
+			'Platform for pharmacies, where the product niche was predominantly focused on animals and veterinarians',
 		link: 'https://www.propecuaria.pt/pt/crm-myprovet',
 		src: '/preview-myprovet.jpg',
 	},
@@ -41,7 +41,7 @@ export const projects = [
 		title: 'Erising',
 		caption: 'Consultancy Company',
 		smallDescription:
-			'A small consultancy company from industry world. They provide consultancy services.',
+			'A small consultancy company from industry world. They provide consultancy services',
 		link: 'https://erising.pt',
 		src: '/preview-erising.jpg',
 	},
@@ -49,7 +49,7 @@ export const projects = [
 		title: 'SCP eSports',
 		caption: 'eSports club from Sporting Clube Portugal',
 		smallDescription:
-			"Sporting Clube Portugal had a gaming team and wanted a website that was similar to the institution's, but with a different style.",
+			"Sporting Clube Portugal had a gaming team and wanted a website that was similar to the institution's, but with a different style",
 		link: 'https://www.sporting.pt/pt/modalidades/esports/atletas',
 		src: '/preview-scp.jpg',
 	},
